6678582b10228306315ca0fd0853ff1337c15d4c
Raul Ferrando <rferrandop@protonmail.com>
2024-05-22 19:50:02 +0200

    test

---
+++ 339A-HelpfulMaths.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  char sum[100];
+  scanf("%s", sum);
+
+  int numbers[100];
+  for (int i = 0; i < 100; i++) {
+    if (sum[i] == '1' || sum[i] == '2' || sum[i] == '3')
+      numbers[i] = atoi(&sum[i]);
+  }
+
+  qsort(numbers, sizeof(numbers)/sizeof(*numbers), sizeof(*numbers), ;
+  int final_sum[100];
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
+      if (sum[i] > sum[j])
+    }
+  }
+}
